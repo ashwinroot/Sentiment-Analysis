9f2e02d2a34762daf0fd8c987620f32ba441e303
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2017-10-18 11:11:46 -0600

    Checking Run

--- getInput.py
+++ getInput.py
@@ -14,33 +14,35 @@ def read_file(filename,dictator):
         dictator[id] =review
     return positive_dict
 
+def get_complete_dictionary():
+    positive_dict = {}
+    negative_dict = {}
+    positive_dict = read_file("Dataset/hotelPosT-train.txt",positive_dict)
+    negative_dict = read_file("Dataset/hotelNegT-train.txt",negative_dict)
 
-positive_dict = {}
-negative_dict = {}
-positive_dict = read_file("Dataset/hotelPosT-train.txt",positive_dict)
-negative_dict = read_file("Dataset/hotelNegT-train.txt",negative_dict)
+    common_dict = defaultdict(dict)
 
-common_dict = defaultdict(dict)
+    for id in positive_dict:
+        sentence = positive_dict[id]
+        word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
+        for word in word_list:
+            if word not in common_dict:
+                common_dict[word]['POS'] = 1
+            else:
+                common_dict[word]['POS'] +=1
+    print(common_dict)
 
-for id in positive_dict:
-    sentence = positive_dict[id]
-    word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
-    for word in word_list:
-        if word not in common_dict:
-            common_dict[word]['POS'] = 1
-        else:
-            common_dict[word]['POS'] +=1
-print(common_dict)
+    for id in negative_dict:
+        sentence = negative_dict[id]
+        word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
+        for word in word_list:
+            if word not in common_dict:
+                common_dict[word]['NEG'] = 1
+            elif 'NEG' not in common_dict[word]:
+                common_dict[word]['NEG'] = 1
+            else:
+                common_dict[word]['NEG'] +=1
+    return common_dict
 
-for id in negative_dict:
-    sentence = negative_dict[id]
-    word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
-    for word in word_list:
-        if word not in common_dict:
-            common_dict[word]['NEG'] = 1
-        elif 'NEG' not in common_dict[word]:
-            common_dict[word]['NEG'] = 1
-        else:
-            common_dict[word]['NEG'] +=1
-print(common_dict)
+
 
