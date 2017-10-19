9b77cbc9b27f0ded3bdcca160b6c007ce2ace7cc
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2017-10-18 22:00:29 -0600

    Naives Bayes
    Working with Laplacian 1 smoothing.

--- getInput.py
+++ getInput.py
@@ -1,6 +1,7 @@
 import re
 from collections import defaultdict
-
+import nltk
+from nltk.corpus import stopwords
 
 def read_file(filename,dictator):
     fp = open(filename)
@@ -12,37 +13,38 @@ def read_file(filename,dictator):
             continue
         id , review = re.split(r'[\t]+',reviews)
         dictator[id] =review
-    return positive_dict
+    return dictator
 
 def get_complete_dictionary():
     positive_dict = {}
     negative_dict = {}
     positive_dict = read_file("Dataset/hotelPosT-train.txt",positive_dict)
     negative_dict = read_file("Dataset/hotelNegT-train.txt",negative_dict)
-
+    positive_words = 0
+    negative_words = 0
     common_dict = defaultdict(dict)
 
     for id in positive_dict:
         sentence = positive_dict[id]
         word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
         for word in word_list:
+            positive_words+=1
             if word not in common_dict:
                 common_dict[word]['POS'] = 1
             else:
                 common_dict[word]['POS'] +=1
-    print(common_dict)
-
     for id in negative_dict:
         sentence = negative_dict[id]
         word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
         for word in word_list:
+            negative_words+=1
             if word not in common_dict:
                 common_dict[word]['NEG'] = 1
             elif 'NEG' not in common_dict[word]:
                 common_dict[word]['NEG'] = 1
             else:
                 common_dict[word]['NEG'] +=1
-    return common_dict
+    return common_dict,positive_dict,negative_dict,positive_words,negative_words
 
 
-
+# def remove_stop_words(se)
