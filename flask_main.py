2cc8b793cd7f2b115440d6ebf3cef827360d5ebe
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2017-10-26 14:25:48 -0600

    Flask Application

--- flask_main.py
+++ flask_main.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import run
+import Sentiment_Classifier
 
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
+    Sentiment_Classifier.train()
     return render_template("index.html")
 
 @app.route('/', methods=['GET','POST'])
 def my_form_post():
     text = request.form['text']
-    output, percentage = run.NB_Classifier(text)
-    return render_template("index.html", output=output,percentage=percentage )
+
+    my_output = Sentiment_Classifier.NB_Classifier(text)
+    nb_output = Sentiment_Classifier.NLTK_NB(text)
+    return render_template("index.html", my_output=my_output,nb_output=nb_output )
 
 
 if __name__ == '__main__':
--- getInput.py
+++ getInput.py
@@ -2,7 +2,13 @@ import re
 from collections import defaultdict
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 
+
+punctuation = [',','.','!','?',';',':','-']
 def read_file(filename,dictator):
     fp = open(filename)
     data = fp.read()
@@ -12,8 +18,10 @@ def read_file(filename,dictator):
         if reviews=='':
             continue
         id , review = re.split(r'[\t]+',reviews)
-        dictator[id] =review
+        dictator[id] = review
     return dictator
+
+
 
 def get_complete_dictionary():
     positive_dict = {}
@@ -28,6 +36,7 @@ def get_complete_dictionary():
         sentence = positive_dict[id]
         word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
         for word in word_list:
+            # word = word.lower()
             positive_words+=1
             if word not in common_dict:
                 common_dict[word]['POS'] = 1
@@ -46,6 +55,8 @@ def get_complete_dictionary():
                 common_dict[word]['NEG'] +=1
     return common_dict,positive_dict,negative_dict,positive_words,negative_words
 
+
+
 def get_complete_dictionary_without_stopwords():
     positive_dict = {}
     negative_dict = {}
@@ -57,33 +68,80 @@ def get_complete_dictionary_without_stopwords():
 
     for id in positive_dict:
         sentence = positive_dict[id]
-        word_list = remove_stop_words(sentence)
+        word_list = remove_stop_words_stem(sentence)
         # word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
         for word in word_list:
-            positive_words+=1
+            positive_words += 1
             if word not in common_dict:
                 common_dict[word]['POS'] = 1
             else:
-                common_dict[word]['POS'] +=1
+                common_dict[word]['POS'] += 1
     for id in negative_dict:
         sentence = negative_dict[id]
-        word_list = remove_stop_words(sentence)
+        word_list = remove_stop_words_stem(sentence)
         # word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
         for word in word_list:
-            negative_words+=1
+            negative_words += 1
             if word not in common_dict:
                 common_dict[word]['NEG'] = 1
             elif 'NEG' not in common_dict[word]:
                 common_dict[word]['NEG'] = 1
             else:
-                common_dict[word]['NEG'] +=1
+                common_dict[word]['NEG'] += 1
     return common_dict,positive_dict,negative_dict,positive_words,negative_words
 
 
-def remove_stop_words(sentence):
+# def positive_negative_dictionary():
+#     positive_dict = {}
+#     negative_dict = {}
+#     positive_dict = read_file("Dataset/hotelPosT-train.txt",positive_dict)
+#     negative_dict = read_file("Dataset/hotelNegT-train.txt",negative_dict)
+#     return positive_dict,negative_dict
+
+'''
+input :  a set of positive reviews and negative reviews
+output : count of positive and negative words, a common dictionary
+'''
+
+def create_complete_dictionary_without_stopwords(positive_dict,negative_dict):
+    positive_words = 0
+    negative_words = 0
+    common_dict = defaultdict(dict)
+
+    for id in positive_dict:
+        sentence = positive_dict[id]
+        word_list = remove_stop_words_stem(sentence)
+        # word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
+        for word in word_list:
+            positive_words += 1
+            if word not in common_dict:
+                common_dict[word]['POS'] = 1
+            else:
+                common_dict[word]['POS'] += 1
+    for id in negative_dict:
+        sentence = negative_dict[id]
+        word_list = remove_stop_words_stem(sentence)
+        # word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
+        for word in word_list:
+            negative_words += 1
+            if word not in common_dict:
+                common_dict[word]['NEG'] = 1
+            elif 'NEG' not in common_dict[word]:
+                common_dict[word]['NEG'] = 1
+            else:
+                common_dict[word]['NEG'] += 1
+    return common_dict,positive_words,negative_words
+
+def remove_stop_words_stem(sentence):
     return_list =[]
+    ls = LancasterStemmer()
     word_list = re.findall(r"[\w']+|[.,!?;]", sentence)
     for word in word_list:
         if word not in set(stopwords.words('english')):
+            word = word.lower()
+            word = ls.stem(word)
+            if word in punctuation:
+                continue
             return_list.append(word)
     return return_list
+
